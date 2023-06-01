0de2dbe95dd3ccf7328724571308b94497dc7f24
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-06-02 05:24:38 +0600

    Manage Items Added In Dashboard

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -2,20 +2,88 @@ import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
-    const [menu] = useMenu();
+
+    // const [menu, , refetch] = useMenu();
+    // const [axiosSecure] = useAxiosSecure();
+
+    const [menu, , refetch] = useMenu();
+    const [axiosSecure] = useAxiosSecure();
+
+
+    // const handleDelete = item => {
+    //     console.log(item);
+    //     Swal.fire({
+    //         title: 'Are you sure?',
+    //         text: "You won't be able to revert this!",
+    //         icon: 'warning',
+    //         showCancelButton: true,
+    //         confirmButtonColor: '#3085d6',
+    //         cancelButtonColor: '#d33',
+    //         confirmButtonText: 'Yes, delete it!'
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             axiosSecure.delete(`/menu/${item._id}`)
+    //                 .then(res => {
+    //                     console.log('deleted response', res);
+    //                     if (res.data.deletedCount > 0) {
+    //                         refetch();
+    //                         Swal.fire({
+    //                             position: 'center-center',
+    //                             icon: 'success',
+    //                             title: `${item.name} is an Admin Now!`,
+    //                             showConfirmButton: false,
+    //                             timer: 1500
+    //                         })
+    //                     }
+    //                 })
+    //         }
+    //     })
+    // }
+
+
+    const handleDelete = item => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/menu/${item._id}`)
+                    .then(res => {
+                        console.log('deleted res', res.data);
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
+
+
+
     return (
         <>
             <SectionTitle subHeading="Hurry Up!" heading="MANAGE ALL ITEMS"></SectionTitle>
             <Helmet>
                 <title>Bistro | Manage Items</title>
             </Helmet>
-            <div className="bg-white p-10 w-3/4">
-                <div className="uppercase text-xl font-bold mb-4 flex justify-evenly items-center">
-                    <h2>Total Orders:</h2>
-                    <h2>Total Price: </h2>
-                    <button className="btn btn-sm bg-[#D1A054] border-0">Pay</button>
+            <div className="bg-white p-4 w-3/4">
+                <div className="uppercase text-xl font-bold mb-4">
+                    <h2>Total Items: {menu.length}</h2>
                 </div>
 
 
@@ -36,30 +104,27 @@ const ManageItems = () => {
                             {/* row 1 */}
 
                             {
-                                menu.map(item => <tr key={item._id}>
-                                    <th></th>
+                                menu.map((item, index) => <tr key={item._id}>
+                                    <th>{index + 1}</th>
                                     <td>
                                         <div className="avatar">
                                             <div className="rounded object-cover w-12 h-12">
-                                                <img src='' alt="Avatar Tailwind CSS Component" />
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
                                     </td>
                                     <td>
-                                        <h2>Name</h2>
+                                        <h2>{item.name}</h2>
                                     </td>
-                                    <td><p>Price</p></td>
+                                    <td><p>${item.price}</p></td>
                                     <td>
                                         <button className="btn bg-[#D1A054] text-white border-0"><FaEdit className="text-lg"></FaEdit></button>
                                     </td>
                                     <td>
-                                        <button className="btn bg-red-700 text-white border-0"><FaTrashAlt className="text-lg"></FaTrashAlt></button>
+                                        <button onClick={() => handleDelete(item)} className="btn bg-red-700 text-white border-0"><FaTrashAlt className="text-lg"></FaTrashAlt></button>
                                     </td>
                                 </tr>)
                             }
-
-
-
                         </tbody>
                     </table>
                 </div>
